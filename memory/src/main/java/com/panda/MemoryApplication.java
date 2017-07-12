3961d5e4b1cc8e1fda15542a0b0e66b51fda4519
panda.huangwei <pandahuangwei@163.com>
2017-07-12 18:51:29 +0800

    init commit

---
+++ memory/src/main/java/com/panda/MemoryApplication.java
@@ -0,0 +1,16 @@
+package com.panda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Panda.HuangWei.
+ * @since 2017-07-12 18:23.
+ */
+@SpringBootApplication
+public class MemoryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MemoryApplication.class, args);
+    }
+}
